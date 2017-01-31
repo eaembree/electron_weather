6652ed96601c577b3e8ffaeaccc2af16165a862a
Eric Embree <eembree@healthcare.uiowa.edu>
2017-01-31 14:40:40 -0600

    Work on weather api

--- renderer.js
+++ renderer.js
@@ -1,3 +1,4 @@
+var $ = jquery = require("jquery")
 const readline = require('readline');
 const fs = require('fs');
 
@@ -7,95 +8,63 @@ const fs = require('fs');
 
 var APPID = '78be9c2134128e5ce421a501669830f1'
 
-var IowaCityId = 4862034;
+// Read cities from file and populate drop down.
+let cityArr = [];
 
-var $ = jquery = require("jquery")
-
-$('h1').html('Hello from jQuery!')
-
-/*
-$.ajax({
-    type: 'GET',
-    crossDomain: true,
-    url: "https://webapps1.healthcare.uiowa.edu/HPINV",
-    success: function(data, textStatus, jqXHR){
-        console.log('success');
-        console.log(data);
-    },
-    error: function(jqXHR, textStatus, errorThrown){
-        console.log('error');
-    },
-    complete: function(jqXHR, textStatus){
-        console.log('complete');
-    }
-});
-*/
-
-
-const rl = readline.createInterface({
+let rl = readline.createInterface({
     input: fs.createReadStream('mycities.json')
 });
 
-let cityArr = [];
+rl.on('line', function (line) {
+    cityArr.push(JSON.parse(line));
+});
 
-rl.on('line', function(line) {
-    console.log(JSON.parse(line));
+rl.on('close', () => {
+    let newHtml = '';
+    cityArr.forEach((city) => {
+        newHtml += `<option value=${city._id}>${city.name}</option>`
+    })
+    $('#cityId').html(newHtml);
+    cityArr = null;
+    rl = null;
 });
 
 
-
-//console.log(process.env);
-
-/*
-$.ajax({
-    type: 'GET',
-    crossDomain: true,
-    url: "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=" + APPID,
-    success: function(data, textStatus, jqXHR){
-        console.log('success method');
-        console.log(data);
-    },
-    error: function(jqXHR, textStatus, errorThrown){
-        console.log('error method');
-        console.log(jqXHR.responseJSON);
-        console.log(textStatus);
-        console.log(errorThrown);
-    },
-    complete: function(jqXHR, textStatus){
-        console.log('complete method');
-    }
+$('#get-data-button').click(function () {
+    getCityData($('#cityId').val());
 });
-*/
-$.ajax({
-    type: 'GET',
-    crossDomain: true,
-    url: "http://api.openweathermap.org/data/2.5/weather?id=" + IowaCityId + "&units=imperial" + "&APPID=" + APPID,
-    success: function(data, textStatus, jqXHR){
-        console.log('success method');
-        console.log(data);
 
-        $('#name').html(data.name);
-        $('#lat').html(data.coord.lat);
-        $('#lon').html(data.coord.lon);
 
-        $('#id').html(data.weather[0].id);
-        $('#main').html(data.weather[0].main);
-        $('#description').html(data.weather[0].description);
-        $('#icon').html(data.weather[0].icon);
+function getCityData(cityId) {
+    $.ajax({
+        type: 'GET',
+        crossDomain: true,
+        url: "http://api.openweathermap.org/data/2.5/weather?id=" + cityId + "&units=imperial" + "&APPID=" + APPID,
+        success: function (data, textStatus, jqXHR) {
+            $('#name').html(data.name);
+            $('#lat').html(data.coord.lat);
+            $('#lon').html(data.coord.lon);
 
-        $('#pressure').html(data.main.pressure);
-        $('#humidity').html(data.main.humidity);
-        $('#temp').html(data.main.temp);
-        $('#temp_min').html(data.main.temp_min);
-        $('#temp_max').html(data.main.temp_max);
-    },
-    error: function(jqXHR, textStatus, errorThrown){
-        console.log('error method');
-        console.log(jqXHR.responseJSON);
-        console.log(textStatus);
-        console.log(errorThrown);
-    },
-    complete: function(jqXHR, textStatus){
-        console.log('complete method');
-    }
-});
+            $('#id').html(data.weather[0].id);
+            $('#main').html(data.weather[0].main);
+            $('#description').html(data.weather[0].description);
+            $('#icon').html(data.weather[0].icon);
+
+            $('#pressure').html(data.main.pressure);
+            $('#humidity').html(data.main.humidity);
+            $('#temp').html(data.main.temp);
+            $('#temp_min').html(data.main.temp_min);
+            $('#temp_max').html(data.main.temp_max);
+
+            $('#weather-content').show();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log('AJAX ERROR');
+            console.log(jqXHR.responseJSON);
+            console.log(textStatus);
+            console.log(errorThrown);
+        },
+        complete: function (jqXHR, textStatus) {
+        }
+    });
+}
