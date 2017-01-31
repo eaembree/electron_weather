59dd1054e810a7bd1995fc1f0ea0c3c17b5539cb
Eric Embree <eembree@healthcare.uiowa.edu>
2017-01-31 11:52:42 -0600

    Work on pulling weather data

--- renderer.js
+++ renderer.js
@@ -1,12 +1,15 @@
+const readline = require('readline');
+const fs = require('fs');
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
 var APPID = '78be9c2134128e5ce421a501669830f1'
 
-var $ = jquery = require("jquery")
+var IowaCityId = 4862034;
 
-console.log($);
+var $ = jquery = require("jquery")
 
 $('h1').html('Hello from jQuery!')
 
@@ -28,6 +31,22 @@ $.ajax({
 });
 */
 
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('mycities.json')
+});
+
+let cityArr = [];
+
+rl.on('line', function(line) {
+    console.log(JSON.parse(line));
+});
+
+
+
+//console.log(process.env);
+
+/*
 $.ajax({
     type: 'GET',
     crossDomain: true,
@@ -35,6 +54,40 @@ $.ajax({
     success: function(data, textStatus, jqXHR){
         console.log('success method');
         console.log(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown){
+        console.log('error method');
+        console.log(jqXHR.responseJSON);
+        console.log(textStatus);
+        console.log(errorThrown);
+    },
+    complete: function(jqXHR, textStatus){
+        console.log('complete method');
+    }
+});
+*/
+$.ajax({
+    type: 'GET',
+    crossDomain: true,
+    url: "http://api.openweathermap.org/data/2.5/weather?id=" + IowaCityId + "&units=imperial" + "&APPID=" + APPID,
+    success: function(data, textStatus, jqXHR){
+        console.log('success method');
+        console.log(data);
+
+        $('#name').html(data.name);
+        $('#lat').html(data.coord.lat);
+        $('#lon').html(data.coord.lon);
+
+        $('#id').html(data.weather[0].id);
+        $('#main').html(data.weather[0].main);
+        $('#description').html(data.weather[0].description);
+        $('#icon').html(data.weather[0].icon);
+
+        $('#pressure').html(data.main.pressure);
+        $('#humidity').html(data.main.humidity);
+        $('#temp').html(data.main.temp);
+        $('#temp_min').html(data.main.temp_min);
+        $('#temp_max').html(data.main.temp_max);
     },
     error: function(jqXHR, textStatus, errorThrown){
         console.log('error method');
